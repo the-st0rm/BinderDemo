c1ddaedb10ac80ecf3ad47ec4b7d2f4426cd379c
Ibrahim M. El-Sayed (the_storm) <eng.ibrahim@aucegypt.edu>
2016-07-22 01:19:24 +0100

    Compiling the Binder to be statically linked

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := binder
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := binder.cpp
-LOCAL_SHARED_LIBRARIES := libutils libcutils libbinder
+LOCAL_STATIC_LIBRARIES := libbinder libutils libcutils
 LOCAL_C_INCLUDES += frameworks/native/include system/core/include
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
-
